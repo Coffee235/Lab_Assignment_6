b8bc598577842c9f39da13650aed6f2cdbcd6583
Jaeden <jaedencorreia@gmail.com>
2024-02-23 16:33:30 -0500

    commit 1

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,60 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#define _CRT_SECURE_NO_WARNINGS 
 
-int search(int numbers[], int low, int high, int value) 
-{
-	return -1;
+int search(int numbers[], int low, int high, int value) {
+    if (low > high) {
+        return -1; // value not found
+    }
+
+    int mid = (low + high) / 2;
+    
+    if (numbers[mid] == value) {
+        return mid; // Found the value at middle index
+    } else if (numbers[mid] > value) {
+        return search(numbers, low, mid - 1, value); // Search left half
+    } else {
+        return search(numbers, mid + 1, high, value); // Search right half
+    }
 }
 
-void printArray(int numbers[], int sz)
-{
-	int i;
-	printf("Number array : ");
-	for (i = 0;i<sz;++i)
-	{
-		printf("%d ",numbers[i]);
-	}
-	printf("\n");
+void printArray(int numbers[], int sz) {
+    printf("Number array : ");
+    for (int i = 0; i < sz; ++i) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
 }
 
-int main(void)
-{
-	int i, numInputs;
-	char* str;
-	float average;
-	int value;
-	int index;
-	int* numArray = NULL;
-	int countOfNums;
-	FILE* inFile = fopen("input.txt","r");
+int main(void) {
+    int numInputs;
+    FILE* inFile;
+    errno_t err = fopen_s(&inFile, "input.txt", "r");
+    if (err != 0 || inFile == NULL) {
+        printf("Failed to open file.\n");
+        return 1; // return an error code
+    }
 
-	fscanf(inFile, " %d\n", &numInputs);
-	
-	while (numInputs-- > 0)
-	{
-		fscanf(inFile, " %d\n", &countOfNums);
-		numArray = (int *) malloc(countOfNums * sizeof(int));
-		average = 0;
-		for (i = 0; i < countOfNums; i++)
-		{
-			fscanf(inFile," %d", &value);
-			numArray[i] = value;
-			average += numArray[i];
-		}
+    err = fscanf_s(inFile, "%d", &numInputs);
+    if (err == EOF) {
+        printf("Failed to read from file.\n");
+        fclose(inFile);
+        return 1; // return an error code
+    }
+    
+    while (numInputs-- > 0) {
+        int countOfNums;
+        err = fscanf_s(inFile, "%d", &countOfNums);
+        if (err == EOF) {
+            printf("Failed to read from file.\n");
+            fclose(inFile);
+            return 1; // return an error code
+        }
+        
+        int* numArray = (int*)malloc(countOfNums * sizeof(int));
+        if (numArray == NULL) {
+            printf("Memory allocation failed.\n");
+            fclose(inFile);
+            return 1; // return an error code
+        }
+        
+        float average = 0;
+        
+        for (int i = 0; i < countOfNums; i++) {
+            err = fscanf_s(inFile, "%d", &numArray[i]);
+            if (err == EOF) {
+                printf("Failed to read from file.\n");
+                fclose(inFile);
+                free(numArray);
+                return 1; // return an error code
+            }
+            average += numArray[i];
+        }
 
-		printArray(numArray, countOfNums);
-		value = average / countOfNums;
-		index = search(numArray, 0, countOfNums - 1, value);
-		if (index >= 0)
-		{
-			printf("Item %d exists in the number array at index %d!\n",value, index);
-		}
-		else
-		{
-			printf("Item %d does not exist in the number array!\n", value);
-		}
+        printArray(numArray, countOfNums);
+        int value = average / countOfNums;
+        int index = search(numArray, 0, countOfNums - 1, value);
+        
+        if (index >= 0) {
+            printf("Item %d exists in the number array at index %d!\n", value, index);
+        } else {
+            printf("Item %d does not exist in the number array!\n", value);
+        }
 
-		free(numArray);
-	}
+        free(numArray);
+    }
 
-	fclose(inFile);
+    fclose(inFile);
+    return 0;
 }
